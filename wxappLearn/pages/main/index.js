874ea8417480a9366f1ebbfa95e1955688f938c0
soulget <48373556+soulget@users.noreply.github.com>
2019-04-24 22:59:27 +0800

    Add files via upload

--- wxappLearn/pages/main/index.js
+++ wxappLearn/pages/main/index.js
@@ -1,11 +1,32 @@
 // pages/main/index.js
 Page({
   data: {
+    city:'南昌',
     imgUrls: [
       'https://images.unsplash.com/photo-1551334787-21e6bd3ab135?w=640',
       'https://images.unsplash.com/photo-1551214012-84f95e060dee?w=640',
       'https://images.unsplash.com/photo-1551446591-142875a901a1?w=640'
     ],
+    items: []
+    },
+    onLoad () {
+      let that = this
+      wx.request({
+        url: 'https://www.easy-mock.com/mock/5ca4580fc4e9a575b66b62b7/example/goodboy',
+        success:function(res){
+          console.log(res);
+          that.setData({
+            items:res.data.data.movieList
+          })
+        }
+      })
+    },
+    onPullDownRefresh(){
+      console.log(1234)
+    },
+
   
-  },
+
+  
+  
 })
