2841c50a8038723710c89b2cb256b6f19d1dbad0
soulget <48373556+soulget@users.noreply.github.com>
2019-05-21 22:37:14 +0800

    Add files via upload

---
+++ wxapp/upload/miniprogram/pages/upload/upload.js
@@ -0,0 +1,50 @@
+const app = getApp()
+
+Page({
+  data:{
+    files:[]
+  },
+  chooseImage(){
+    let that = this
+    wx.chooseImage({
+      sizeType:['original', 'compressed'],
+      sourceType:['album', 'camera'],
+      success(res){
+        console.log(res)
+        that.setData({
+          files: that.data.files.concat(res.tempFilePaths)
+        })
+        //-------------
+        for(let i = 0; i < res.tempFilePaths.length; i++){
+          const filePath = res.tempFilePaths[i]
+          let a = filePath.lastIndexOf('.')
+          let b = filePath.lastIndexOf('.', filePath.length-7)
+          let c = filePath.substring(b+1, a)
+          const cloudPath = c + filePath.match(/\.[^.]+?$/)
+        
+          wx.cloud.uploadFile({
+            cloudPath,
+            filePath,// 文件路径
+            success: res => {
+              // get resource ID
+             
+              console.log('上传成功',res)
+            },
+            fail: err => {
+              // handle error
+            }
+          })
+        }
+      
+       
+      }
+    })
+  },
+  previewImage(e){
+    console.log(e)
+    wx.previewImage({
+      current: e.currentTarget.id,
+      urls: this.data.files
+    })
+  }
+})
