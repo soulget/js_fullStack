4627143489effe72a913da360ae1028cca1b9269
soulget <48373556+soulget@users.noreply.github.com>
2019-05-26 23:00:46 +0800

    Add files via upload

---
+++ js/func_args/demo1.js
@@ -0,0 +1,7 @@
+function information(name, sex, money, birthday, color, likes, follow, age){
+    return `${name} ${sex} ${money} ${birthday} ${color} ${likes} ${follow} ${age}`
+}
+
+let user = {name: '李丹', sex:'女', money:'1个亿', birthday:'5-22', color:'yeoolw', likes:['看剧'], follow:'周杰伦', age:'18'};
+// let name = name;
+information(user);
---
+++ js/func_args/demo2.js
@@ -0,0 +1,5 @@
+function information({name, age, height}) {
+    console.log(name, age, height);
+}
+let user = {name:'付豪', gae:19, height:180, money:'五个亿', sex:'男'};
+information();
